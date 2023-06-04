4f20e37e83d2fbf25ef048bc63a3788bc016178f
Mohit Verma <112820522+mohitvdx@users.noreply.github.com>
2023-06-04 20:41:49 +0530

    added all files

---
+++ hardhat-tutorial/contracts/Whitelist.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract Whitelist {
+
+    uint8 public maxWhitelistedAddresses;
+
+    mapping (address => bool) public whitelistedAddresses;
+
+    uint public numwhitelistedAddresses;
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
+    }
+
+    function addAddressToWhitelist(address _address) public {
+        require(whitelistedAddresses[msg.sender], "You aren't whitelisted");
+        require(!whitelistedAddresses[_address], "Address is already whitelisted");
+        whitelistedAddresses[_address] = true;
+
+        numwhitelistedAddresses +=1 ;
+    }
+}
