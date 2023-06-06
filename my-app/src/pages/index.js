d7066d9133b22ca87fd1e5d9166dce0d41a7620a
Mohit Verma <112820522+mohitvdx@users.noreply.github.com>
2023-06-06 17:55:07 +0530

    changed goerli to sepolia

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -38,8 +38,8 @@ export default function Home() {
     // If user is not connected to the Goerli network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
     if (chainId !== 5) {
-      window.alert("Change the network to Goerli");
-      throw new Error("Change network to Goerli");
+      window.alert("Change the network to sepolia");
+      throw new Error("Change network to sepolia");
     }
 
     if (needSigner) {
@@ -182,7 +182,7 @@ export default function Home() {
       // Assign the Web3Modal class to the reference object by setting it's `current` value
       // The `current` value is persisted throughout as long as this page is open
       web3ModalRef.current = new Web3Modal({
-        network: "goerli",
+        network: "sepolia",
         providerOptions: {},
         disableInjectedProvider: false,
       });
